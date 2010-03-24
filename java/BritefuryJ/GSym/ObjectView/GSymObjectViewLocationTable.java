8d8e7fda84afafad89e25c2d7138c185ea44b6ac
britefury <britefury@234abbfc-b52f-0410-82fe-cce727f8316c>
2010-03-24 17:17:10 +0000

    Added queueRefresh methods to GSymFragmentViewContext and IncrementalTreeNode. Added ObjectView package, which contains the beginnings of a general object presentation system.

---
+++ java/BritefuryJ/GSym/ObjectView/GSymObjectViewLocationTable.java
@@ -0,0 +1,53 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008.
+//##************************
+package BritefuryJ.GSym.ObjectView;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+public class GSymObjectViewLocationTable
+{
+	private WeakHashMap<Object, String> objectToLocation = new WeakHashMap<Object, String>();
+	private HashMap<String, WeakReference<Object>> locationToObject = new HashMap<String, WeakReference<Object>>();
+	int objectCount = 1;
+	
+	
+	public GSymObjectViewLocationTable()
+	{
+	}
+	
+	
+	public String getLocationForObject(Object x)
+	{
+		String location = objectToLocation.get( x );
+		if ( location == null )
+		{
+			location = "obj" + objectCount++;
+			objectToLocation.put( x, location );
+		}
+		return location;
+	}
+	
+	public Object getObjectAtLocation(String location)
+	{
+		WeakReference<Object> ref = locationToObject.get( location );
+		if ( ref != null )
+		{
+			Object x = ref.get();
+			
+			if ( x == null )
+			{
+				locationToObject.remove( location );
+			}
+			
+			return x;
+		}
+		
+		return null;
+	}
+}
---
+++ java/BritefuryJ/GSym/ObjectView/Presentable.java
@@ -0,0 +1,15 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008.
+//##************************
+package BritefuryJ.GSym.ObjectView;
+
+import BritefuryJ.DocPresent.DPElement;
+import BritefuryJ.GSym.View.GSymFragmentViewContext;
+
+public interface Presentable
+{
+	public DPElement present(GSymFragmentViewContext ctx, Object state);
+}
