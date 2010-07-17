6275f5f9a9d389055e47dec876a3d8810209522b
britefury <britefury@234abbfc-b52f-0410-82fe-cce727f8316c>
2010-07-17 10:53:42 +0000

    Added more layout combinators.

--- java/BritefuryJ/LayoutCombinators/VExpand.java
+++ java/BritefuryJ/LayoutCombinators/VExpand.java
@@ -6,42 +6,21 @@
 //##************************
 package BritefuryJ.LayoutCombinators;
 
-import org.python.core.PyObject;
-
 import BritefuryJ.AttributeTable.AttributeTable;
 import BritefuryJ.DocPresent.DPElement;
-import BritefuryJ.DocPresent.StyleSheet.PrimitiveStyleSheet;
 import BritefuryJ.GSym.GenericPerspective.GenericPerspectiveStyleSheet;
 import BritefuryJ.GSym.View.GSymFragmentView;
 
-public class Column extends Sequence
+public class VExpand extends Alignment
 {
-	protected PrimitiveStyleSheet styleSheet;
-
-	
-	public Column(Object[] children)
+	public VExpand(Object child)
 	{
-		super( children );
-		styleSheet = PrimitiveStyleSheet.instance;
+		super( child );
 	}
-
-	public Column(PyObject[] values)
-	{
-		super( values );
-		styleSheet = PrimitiveStyleSheet.instance;
-	}
-	
-	
-	public Column withStyle(PrimitiveStyleSheet styleSheet)
-	{
-		this.styleSheet = styleSheet;
-		return this;
-	}
-	
 
 	@Override
 	public DPElement present(GSymFragmentView fragment, GenericPerspectiveStyleSheet styleSheet, AttributeTable inheritedState)
 	{
-		return this.styleSheet.vbox( mapPresentChildren( children, fragment, styleSheet, inheritedState ) );
+		return presentChild( child, fragment, styleSheet, inheritedState ).alignVExpand();
 	}
 }
