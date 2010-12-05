e8c4d17dbc7c923ff8f88dc01ff754a308a56dcb
britefury <britefury@234abbfc-b52f-0410-82fe-cce727f8316c>
2010-12-05 11:39:36 +0000

    Added ModelAccess package, which should have been added last time.

---
+++ java/BritefuryJ/ModelAccess/DocModel/StringToIntegerReader.java
@@ -0,0 +1,47 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008-2010.
+//##************************
+package BritefuryJ.ModelAccess.DocModel;
+
+import BritefuryJ.ModelAccess.ModelReader;
+
+public class StringToIntegerReader extends DMModelReader
+{
+	private ModelReader innerReader;
+	private int defaultValue;
+	
+	
+	public StringToIntegerReader(ModelReader innerReader, int defaultValue)
+	{
+		this.innerReader = innerReader;
+		this.defaultValue = defaultValue;
+	}
+	
+	public StringToIntegerReader(ModelReader innerReader)
+	{
+		this( innerReader, 0 );
+	}
+	
+	
+	
+	@Override
+	public Object readFromModel(Object model)
+	{
+		Object value = innerReader.readFromModel( model );
+		int v = defaultValue;
+		if ( value != null  &&  value instanceof String )
+		{
+			try
+			{
+				v = Integer.valueOf( (String)value );
+			}
+			catch (NumberFormatException e)
+			{
+			}
+		}
+		return v;
+	}
+}
