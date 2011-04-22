63580e67f18691d7c4a2c6ec86a9629e442179a6
britefury <britefury@234abbfc-b52f-0410-82fe-cce727f8316c>
2011-04-22 15:26:48 +0000

    Added change history which failed to commit previously.

---
+++ java/BritefuryJ/ChangeHistory/Change.java
@@ -0,0 +1,44 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008.
+//##************************
+package BritefuryJ.ChangeHistory;
+
+import BritefuryJ.AttributeTable.SimpleAttributeTable;
+import BritefuryJ.DefaultPerspective.Presentable;
+import BritefuryJ.IncrementalView.FragmentView;
+import BritefuryJ.Pres.Pres;
+import BritefuryJ.Pres.Primitive.Label;
+
+public abstract class Change implements Presentable
+{
+	public static class CannotJoinCommandException extends RuntimeException
+	{
+		private static final long serialVersionUID = 1L;
+	}
+	
+	
+	protected abstract void execute();
+	protected abstract void unexecute();
+	protected abstract String getDescription();
+	
+	
+	protected boolean canMergeFrom(Change change)
+	{
+		return false;
+	}
+	
+	protected void mergeFrom(Change change)
+	{
+		throw new CannotJoinCommandException();
+	}
+
+	
+	@Override
+	public Pres present(FragmentView fragment, SimpleAttributeTable inheritedState)
+	{
+		return new Label( getDescription() );
+	}
+}
