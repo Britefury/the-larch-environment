fbb15e66f8ce52a1fce75210e8d76cd7521bcb8a
britefury <britefury@234abbfc-b52f-0410-82fe-cce727f8316c>
2011-06-14 09:28:58 +0000

    Added parser benchmark tests.

---
+++ java/tests/Benchmarks/Parser/ParserProfileTest.java
@@ -0,0 +1,57 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008.
+//##************************
+package tests.Benchmarks.Parser;
+
+import BritefuryJ.Parser.Literal;
+import BritefuryJ.Parser.ParserExpression;
+import BritefuryJ.Parser.Production;
+import BritefuryJ.Parser.Production.CannotOverwriteProductionExpressionException;
+
+public class ParserProfileTest
+{
+	public static double timeIt(ParserExpression parser, String sourceText)
+	{
+		long t1 = System.nanoTime();
+		parser.parseStringChars( sourceText );
+		long t2 = System.nanoTime();
+		return ( t2 - t1 ) / 1000000000.0;
+	}
+	
+	
+	public static void main(String args[]) throws CannotOverwriteProductionExpressionException
+	{
+		Production A = new Production( "A" );
+		Production B = new Production( "B" );
+		A.setExpression( B.__add__( new Literal( "x" ) ).__or__( new Literal( "x" ) ) );
+		B.setExpression( A );
+		
+		ParserExpression parser = A;
+		
+		StringBuilder testSource = new StringBuilder();
+		for (int i = 0; i < 500000; i++)
+		{
+			testSource.append( "x" );
+		}
+		
+		// Warm up
+		timeIt( parser, testSource.toString() );
+		// Run
+		double time = timeIt( parser, testSource.toString() );
+		
+		System.out.println( "Parsing " + testSource.length() + " characters using a left-recursive parser took " + time + " seconds; " + testSource.length() / time + " characters per second." );
+
+	
+		ParserExpression parser2 = new Literal( "x" ).zeroOrMore();
+		
+		// Warm up
+		timeIt( parser2, testSource.toString() );
+		// Run
+		double time2 = timeIt( parser2, testSource.toString() );
+
+		System.out.println( "Parsing " + testSource.length() + " characters using a repeating parser took " + time2 + " seconds; " + testSource.length() / time2 + " characters per second." );
+	}
+}
---
+++ java/tests/Benchmarks/Parser/ParserSpeedTest.java
@@ -0,0 +1,115 @@
+//##* This program is free software; you can use it, redistribute it and/or modify it
+//##* under the terms of the GNU General Public License version 2 as published by the
+//##* Free Software Foundation. The full text of the GNU General Public License
+//##* version 2 can be found in the file named 'COPYING' that accompanies this
+//##* program. This source code is (C)copyright Geoffrey French 2008.
+//##************************
+package tests.Benchmarks.Parser;
+
+import BritefuryJ.Parser.Literal;
+import BritefuryJ.Parser.ParserExpression;
+import BritefuryJ.Parser.Production;
+import BritefuryJ.Parser.Production.CannotOverwriteProductionExpressionException;
+
+public class ParserSpeedTest
+{
+	private static class Result
+	{
+		int numChars;
+		double time;
+		
+		Result(int numChars, double time)
+		{
+			this.numChars = numChars;
+			this.time = time;
+		}
+		
+		
+		
+		public String toString()
+		{
+			return String.valueOf( numChars ) + "\t" + String.valueOf( time );
+		}
+		
+		
+		static Result avg(Result results[])
+		{
+			double tAccum = 0.0;
+			for (Result r: results)
+			{
+				tAccum += r.time;
+			}
+			return new Result( results[0].numChars, tAccum / results.length );
+		}
+	}
+	
+	
+	public static double timeIt(ParserExpression parser, String sourceText)
+	{
+		long t1 = System.nanoTime();
+		parser.parseStringChars( sourceText );
+		long t2 = System.nanoTime();
+		return ( t2 - t1 ) / 1000000000.0;
+	}
+	
+	public static void timeAll(ParserExpression parser, int warmupRepeats, int numRepeats, int stepSize, int maxLength)
+	{
+		StringBuilder sourceText = new StringBuilder();
+		for (int i = 0; i <= maxLength; i++)
+		{
+			sourceText.append( "x" );
+			
+			if ( i >= stepSize )
+			{
+				if ( i % stepSize == 0 )
+				{
+					String source = sourceText.toString();
+					for (int j = 0; j < numRepeats; j++)
+					{
+						timeIt( parser, source );
+					}
+
+					Result results[] = new Result[numRepeats];
+					for (int j = 0; j < numRepeats; j++)
+					{
+						double time = timeIt( parser, source );
+						results[j] = new Result( i, time );
+					}
+					
+					System.out.println( Result.avg( results ) );
+				}
+			}
+		}
+	}
+	
+	
+	public static void main(String args[]) throws CannotOverwriteProductionExpressionException
+	{
+		Production A = new Production( "A" );
+		Production B = new Production( "B" );
+		A.setExpression( B.__add__( new Literal( "x" ) ).__or__( new Literal( "x" ) ) );
+		B.setExpression( A );
+		
+		ParserExpression parser = A;
+		
+		System.out.println( "Warm up..." );
+
+		String testSource = "";
+		for (int i = 0; i < 100; i++)
+		{
+			testSource += "x";
+		}
+		for (int i = 0; i < 100; i++)
+		{
+			parser.parseStringChars( testSource );
+		}
+
+		System.out.println( "Running..." );
+		System.out.println( "#chars\tavg time" );
+		
+		
+		// arguments:
+		//   parser, warmupRepeats, repeats, stepSize, maxLength
+		timeAll( parser, 10, 20, 500, 25000 );
+	}
+}
