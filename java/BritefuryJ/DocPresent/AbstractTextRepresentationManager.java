3c3630e982b3a80f9f199687d3bb259b67885cf5
Britefury <Britefury@localhost>
2011-08-09 18:58:14 +0100

    DPElement.getLeafAtPositionInSubtree() and DPElement.getPositionOfElementInSubtree() now use the DefaultTextRepresentationManager for their functionality. All text representation methods in the DPElement hierarchy now use the default text representation manager for the functionality.

--- java/BritefuryJ/DocPresent/AbstractTextRepresentationManager.java
+++ java/BritefuryJ/DocPresent/AbstractTextRepresentationManager.java
@@ -137,6 +137,211 @@ public abstract class AbstractTextRepresentationManager
 	
 	
 	
+	protected static class FoundException extends RuntimeException
+	{
+		private static final long serialVersionUID = 1L;
+	}
+	
+	
+	protected abstract class FinderVisitor extends ElementTreeVisitor
+	{
+		protected int position = 0;
+		
+		
+		//
+		// VISITOR METHODS
+		//
+		
+		private void append(int length)
+		{
+			if ( length != -1 )
+			{
+				position += length;
+			}
+		}
+		
+		@Override
+		protected void preOrderVisitElement(DPElement e, boolean complete)
+		{
+			append( getElementPrefixLength( e, complete ) );
+		}
+
+		@Override
+		protected void inOrderCompletelyVisitElement(DPElement e)
+		{
+			int length = getElementContentLength( e );
+			if ( length != -1 )
+			{
+				position += length;
+			}
+			else
+			{
+				String value = cache.get( e );
+				if ( value != null )
+				{
+					position += value.length();
+				}
+			}
+		}
+
+		@Override
+		protected void postOrderVisitElement(DPElement e, boolean complete)
+		{
+			append( getElementSuffixLength( e, complete ) );
+		}
+
+		@Override
+		protected void inOrderVisitPartialContentLeafEditable(DPContentLeafEditable e, int startIndex, int endIndex)
+		{
+			append( getPartialEditableLeafContentLength( e, startIndex, endIndex ) );
+		}
+
+		@Override
+		protected boolean shouldVisitChildrenOfElement(DPElement e, boolean completeVisit)
+		{
+			return !cache.containsKey( e );
+		}
+
+		@Override
+		public void visitTextSelection(TextSelection s)
+		{
+			try
+			{
+				super.visitTextSelection( s );
+			}
+			catch (FoundException e)
+			{
+			}
+		}
+
+		@Override
+		public void visitFromStartOfRootToMarker(Marker marker, DPElement root)
+		{
+			try
+			{
+				super.visitFromStartOfRootToMarker( marker, root );
+			}
+			catch (FoundException e)
+			{
+			}
+		}
+
+		@Override
+		public void visitFromMarkerToEndOfRoot(Marker marker, DPElement root)
+		{
+			try
+			{
+				super.visitFromMarkerToEndOfRoot( marker, root );
+			}
+			catch (FoundException e)
+			{
+			}
+		}
+
+		@Override
+		public void visitSubtree(DPElement root)
+		{
+			try
+			{
+				super.visitSubtree( root );
+			}
+			catch (FoundException e)
+			{
+			}
+		}
+	}
+	
+	
+	protected class FindPositionOfElementVisitor extends FinderVisitor
+	{
+		private DPElement elementToFind;
+		private int positionOfElement = -1;
+		
+		
+		
+		public FindPositionOfElementVisitor(DPElement elementToFind)
+		{
+			this.elementToFind = elementToFind;
+		}
+		
+		
+		public int getPosition()
+		{
+			return positionOfElement;
+		}
+		
+		
+		
+		private void testElement(DPElement e)
+		{
+			if ( e == elementToFind )
+			{
+				positionOfElement = position;
+				throw new FoundException();
+			}
+		}
+		
+		
+		@Override
+		protected void preOrderVisitElement(DPElement e, boolean complete)
+		{
+			testElement( e );
+			super.preOrderVisitElement( e, complete );
+		}
+
+
+		@Override
+		protected void inOrderCompletelyVisitElement(DPElement e)
+		{
+			testElement( e );
+			super.inOrderCompletelyVisitElement( e );
+		}
+
+		
+		@Override
+		protected void inOrderVisitPartialContentLeafEditable(DPContentLeafEditable e, int startIndex, int endIndex)
+		{
+			testElement( e );
+			super.inOrderVisitPartialContentLeafEditable( e, startIndex, endIndex );
+		}
+	}
+
+	
+	
+	protected class FindLeafAtPositionVisitor extends FinderVisitor
+	{
+		private int queryPosition;
+		private DPContentLeaf leaf = null;
+		
+		
+		
+		public FindLeafAtPositionVisitor(int queryPosition)
+		{
+			this.queryPosition = queryPosition;
+		}
+		
+		
+		public DPContentLeaf getLeaf()
+		{
+			return leaf;
+		}
+		
+		
+		@Override
+		protected void postOrderVisitElement(DPElement e, boolean complete)
+		{
+			super.postOrderVisitElement( e, complete );
+			if ( queryPosition < position  &&  e instanceof DPContentLeaf )
+			{
+				leaf = (DPContentLeaf)e;
+				throw new FoundException();
+			}
+		}
+	}
+
+	
+	
+	
 	protected ElementValueCache<String> cache;
 
 	
@@ -156,33 +361,52 @@ public abstract class AbstractTextRepresentationManager
 	
 	
 	
-	public String getTextRepresentationOf(DPElement root)
+	public String getTextRepresentationOf(DPElement subtreeRoot)
 	{
-		BuilderVisitor builder = new BuilderVisitor();
-		builder.visitSubtree( root );
-		return builder.getValue();
+		BuilderVisitor visitor = new BuilderVisitor();
+		visitor.visitSubtree( subtreeRoot );
+		return visitor.getValue();
 	}
 
-	public String getTextRepresentationFromStartToMarker(Marker marker, DPElement root)
+	public String getTextRepresentationFromStartToMarker(Marker marker, DPElement subtreeRoot)
 	{
-		BuilderVisitor builder = new BuilderVisitor();
-		builder.visitFromStartOfRootToMarker( marker, root );
-		return builder.getValue();
+		BuilderVisitor visitor = new BuilderVisitor();
+		visitor.visitFromStartOfRootToMarker( marker, subtreeRoot );
+		return visitor.getValue();
 	}
 
-	public String getTextRepresentationFromMarkerToEnd(Marker marker, DPElement root)
+	public String getTextRepresentationFromMarkerToEnd(Marker marker, DPElement subtreeRoot)
 	{
-		BuilderVisitor builder = new BuilderVisitor();
-		builder.visitFromMarkerToEndOfRoot( marker, root );
-		return builder.getValue();
+		BuilderVisitor visitor = new BuilderVisitor();
+		visitor.visitFromMarkerToEndOfRoot( marker, subtreeRoot );
+		return visitor.getValue();
 	}
 
 	public String getTextRepresentationInTextSelection(TextSelection selection)
 	{
-		BuilderVisitor builder = new BuilderVisitor();
-		builder.visitTextSelection( selection );
-		return builder.getValue();
+		BuilderVisitor visitor = new BuilderVisitor();
+		visitor.visitTextSelection( selection );
+		return visitor.getValue();
 	}
+	
+	
+	
+	public DPContentLeaf getLeafAtPositionInSubtree(DPElement subtreeRoot, int position)
+	{
+		FindLeafAtPositionVisitor visitor = new FindLeafAtPositionVisitor( position );
+		visitor.visitSubtree( subtreeRoot );
+		return visitor.getLeaf();
+	}
+	
+	public int getPositionOfElementInSubtree(DPContainer subtreeRoot, DPElement e)
+	{
+		FindPositionOfElementVisitor visitor = new FindPositionOfElementVisitor( e );
+		visitor.visitSubtree( subtreeRoot );
+		return visitor.getPosition();
+	}
+	
+	
+	
 
 	
 	
@@ -205,5 +429,30 @@ public abstract class AbstractTextRepresentationManager
 	protected final String getPartialEditableLeafContent(DPContentLeafEditable e, int startIndex, int endIndex)
 	{
 		return e.getLeafTextRepresentation().substring( startIndex, endIndex );
+	}
+	
+	
+	
+	protected int getElementPrefixLength(DPElement e, boolean complete)
+	{
+		String prefix = getElementPrefix( e, complete );
+		return prefix != null  ?  prefix.length()  :  -1;
+	}
+
+	protected int getElementSuffixLength(DPElement e, boolean complete)
+	{
+		String prefix = getElementSuffix( e, complete );
+		return prefix != null  ?  prefix.length()  :  -1;
+	}
+
+	protected int getElementContentLength(DPElement e)
+	{
+		String prefix = getElementContent( e );
+		return prefix != null  ?  prefix.length()  :  -1;
+	}
+	
+	protected int getPartialEditableLeafContentLength(DPContentLeafEditable e, int startIndex, int endIndex)
+	{
+		return endIndex - startIndex;
 	}
 }
