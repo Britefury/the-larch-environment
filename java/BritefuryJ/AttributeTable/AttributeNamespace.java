a370246c3e5323646164e1eaa3b6ce8f874b1253
Geoff <Geoff@Britefury>
2011-08-24 14:48:26 +0100

    Attributes, and attribute namespaces are now presentable. Object list table editors now handle unsupported operation exceptions caused by attempting to modify read-only data.

--- java/BritefuryJ/AttributeTable/AttributeNamespace.java
+++ java/BritefuryJ/AttributeTable/AttributeNamespace.java
@@ -6,13 +6,28 @@
 //##************************
 package BritefuryJ.AttributeTable;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import org.python.core.Py;
 
+import BritefuryJ.DefaultPerspective.Presentable;
+import BritefuryJ.DocPresent.Border.SolidBorder;
+import BritefuryJ.IncrementalView.FragmentView;
+import BritefuryJ.Pres.Pres;
+import BritefuryJ.Pres.ObjectPres.ObjectBox;
+import BritefuryJ.Pres.Primitive.Column;
+import BritefuryJ.Pres.Primitive.Label;
+import BritefuryJ.Pres.Primitive.Primitive;
+import BritefuryJ.Pres.Primitive.Row;
+import BritefuryJ.Pres.Primitive.Table;
+import BritefuryJ.StyleSheet.StyleSheet;
 
-public class AttributeNamespace
+
+public class AttributeNamespace implements Presentable
 {
 	protected static Pattern validNamePattern = Pattern.compile( "[a-zA-Z_][a-zA-Z0-9_]*" );
 
@@ -91,8 +106,50 @@ public class AttributeNamespace
 	}
 	
 	
+	
+	@Override
+	public Pres present(FragmentView fragment, SimpleAttributeTable inheritedState)
+	{
+		initStyles();
+		
+		ArrayList<String> attrNames = new ArrayList<String>();
+		attrNames.addAll( attributes.keySet() );
+		Collections.sort( attrNames );
+
+		Pres title = new Row( new Object[] { namespaceStyle.applyTo( new Label( "Name: " ) ), namespaceNameStyle.applyTo( new Label( name ) ) } );
+		
+		Object tableContents[][] = new Object[attrNames.size()+1][];
+		tableContents[0] = new Object[] { headingStyle.applyTo( new Label( "Attribute name" ) ), headingStyle.applyTo( new Label( "Default value" ) ) };
+		for (int i = 0; i < attrNames.size(); i++)
+		{
+			String name = attrNames.get( i );
+			tableContents[i+1] = new Object[] { attrNameStyle.applyTo( new Label( name ) ), attributes.get( name ).getDefaultValue() };
+		}
+		Pres table = tableStyle.applyTo( new Table( tableContents ) );
+		
+		return new ObjectBox( "Attribute namespace", new Column( new Object[] { title, table } ) );
+	}
+
+
+
 	public String toString()
 	{
 		return name;
 	}
+	
+	
+	private static void initStyles()
+	{
+		if ( namespaceStyle == null  ||  namespaceNameStyle == null  ||  headingStyle == null  ||  attrNameStyle == null  ||  tableStyle == null )
+		{
+			namespaceStyle = StyleSheet.style( Primitive.foreground.as( new Color( 0.3f, 0.3f, 0.4f ) ) );
+			namespaceNameStyle = StyleSheet.style( Primitive.fontSize.as( 14 ), Primitive.fontBold.as( true ), Primitive.foreground.as( new Color( 0.0f, 0.0f, 0.4f ) ) );
+			headingStyle = StyleSheet.style( Primitive.fontSize.as( 14 ), Primitive.fontBold.as( true ) );
+			attrNameStyle = StyleSheet.style( Primitive.foreground.as( new Color( 0.0f, 0.4f, 0.4f ) ) );
+			tableStyle = StyleSheet.style( Primitive.tableCellBoundaryPaint.as( new Color( 0.3f, 0.3f, 0.3f ) ), Primitive.tableBorder.as( new SolidBorder() ) );
+		}
+	}
+	
+	
+	private static StyleSheet namespaceStyle = null, namespaceNameStyle = null, headingStyle = null, attrNameStyle = null, tableStyle = null;
 }
