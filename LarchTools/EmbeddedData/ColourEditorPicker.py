42a943be7927dbb032ba66ff64516becd786a5bc
Geoff <Geoff@Britefury>
2012-03-01 15:09:34 +0000

    Factored colour picker out of java.awt.Color cell editor into the new colour picker control. Added a module to the embedded data system for embedding colours into code.

---
+++ LarchTools/EmbeddedData/ColourEditorPicker.py
@@ -0,0 +1,31 @@
+##-*************************
+##-* This program is free software; you can use it, redistribute it and/or modify it
+##-* under the terms of the GNU General Public License version 2 as published by the
+##-* Free Software Foundation. The full text of the GNU General Public License
+##-* version 2 can be found in the file named 'COPYING' that accompanies this
+##-* program. This source code is (C)copyright Geoffrey French 1999-2011.
+##-*************************
+__author__ = 'Geoff'
+
+from java.awt import Color
+
+from BritefuryJ.Controls import ColourPicker
+
+from LarchTools.EmbeddedData.Model import Model
+from LarchTools.EmbeddedData.Editor import Editor
+
+
+
+class ColourEditorPicker (Editor):
+	def __init__(self, model=None, value=Color.GRAY):
+		super( ColourEditorPicker, self ).__init__( model, value )
+
+
+	def _newModel(self, value):
+		if not isinstance( value, Color ):
+			value = Color.GRAY
+		return Model( value )
+
+
+	def __present__(self, fragment, inheritedState):
+		return ColourPicker( self._model.liveValue )
