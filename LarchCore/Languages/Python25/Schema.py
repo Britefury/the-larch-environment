23b45977ac5144a40f21858298e81bcafb391c9c
Geoff <Geoff@Britefury.dlink.com>
2012-03-20 18:48:23 +0000

    Slight visual improvements to visual regular expression editor. Multiline string literal added to Python schema.

--- LarchCore/Languages/Python25/Schema.py
+++ LarchCore/Languages/Python25/Schema.py
@@ -34,6 +34,7 @@ UNPARSED = schema.newClass( 'UNPARSED', Node, [ 'value' ] )
 #
 Literal = schema.newClass( 'Literal', Expr, [] )
 StringLiteral = schema.newClass( 'StringLiteral', Literal, [ 'format', 'quotation', 'value' ] )
+MultilineStringLiteral = schema.newClass( 'MultilineStringLiteral', Literal, [ 'format', 'quotation', 'value' ] )
 IntLiteral = schema.newClass( 'IntLiteral', Literal, [ 'format', 'numType', 'value' ] )
 FloatLiteral = schema.newClass( 'FloatLiteral', Literal, [ 'value' ] )
 ImaginaryLiteral = schema.newClass( 'ImaginaryLiteral', Literal, [ 'value' ] )
