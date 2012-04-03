bc9bbf3d8947aaf956f278ebddb0e0bc3b5e5ffa
Britefury <Britefury@localhost>
2012-04-03 11:05:04 +0100

    Refactored import hooks and module loading logic so that name splitting does not have to be done by each loader in the sequence.

--- LarchCore/Project/ProjectEditor/ModuleFinder.py
+++ LarchCore/Project/ProjectEditor/ModuleFinder.py
@@ -11,18 +11,62 @@ from LarchCore.Project.ProjectPackage import ProjectPackage
 from LarchCore.Project.ProjectPage import ProjectPage
 
 
-class _PackageLoader (object):
-	"""
-	A module loader for loading project packages
-	Looks for an __init__ module, and attempts to load that.
-	"""
-	def __init__(self, projectSubject, packageLocation, package, document):
-		self._projectSubject = projectSubject
-		self._packageLocation = packageLocation
-		self._package = package
-		self._document = document
-		
+class PageFinder (object):
+	def __init__(self, moduleLoader):
+		self._moduleLoader = moduleLoader
 
+
+	def find_module(self, name, fullname, path):
+		return None
+
+
+	def load_module(self, fullname):
+		return self._moduleLoader.load_module( fullname )
+
+
+
+
+
+class PackageFinder (object):
+	def __init__(self, projectSubject, model, modelLocation):
+		self._projectSubject = projectSubject
+		if model is None:
+			model = self._projectSubject._model
+		if modelLocation is None:
+			modelLocation = self._projectSubject._location
+		self._model = model
+		self._modelLocation = modelLocation
+		self._document = self._projectSubject._document
+
+
+	def find_module(self, name, fullname, path):
+		item = self._model.contentsMap.get( name )
+
+		if item is not None:
+			modelLocation = self._modelLocation + '.' + name
+			model = item
+
+			if isinstance( model, ProjectPage ):
+				# We have found a page: get its subject
+				pageSubject = self._projectSubject._document.newModelSubject( model.data, self._projectSubject, modelLocation, fullname, name )
+				# Now, check if it has a 'createModuleLoader' method - if it has, then we can use it. Otherwise, we can't
+				try:
+					createModuleLoader = pageSubject.createModuleLoader
+				except AttributeError:
+					return None
+				else:
+					# The subject has a 'createModuleLoader' attribute - invoke it to create the module loader, for the module import system to use
+					return PageFinder( createModuleLoader( self._document ) )
+			elif isinstance( model, ProjectPackage ):
+				return PackageFinder( self._projectSubject, model, modelLocation )
+			else:
+				raise TypeError, 'unrecognised model type'
+		else:
+			return None
+
+
+
+	# Package module loading
 	def load_module(self, fullname):
 		try:
 			return sys.modules[fullname]
@@ -30,11 +74,11 @@ class _PackageLoader (object):
 			pass
 
 		# First, see if there is an '__init__; page
-		initPage = self._package.contentsMap.get( '__init__' )
-		
+		initPage = self._model.contentsMap.get( '__init__' )
+
 		if initPage is not None and isinstance( initPage, ProjectPage ):
 			# We have found a page called '__init__' - get its subject
-			modelLocation = self._packageLocation + '.__init__'
+			modelLocation = self._modelLocation + '.__init__'
 			pageSubject = self._projectSubject._document.newModelSubject( initPage.data, self._projectSubject, modelLocation, fullname + '.__init__', '__init__' )
 			# Now, check if it has a 'createModuleLoader' method - if it has, then we can use it. Otherwise, use the default
 			try:
@@ -44,68 +88,31 @@ class _PackageLoader (object):
 			else:
 				loader = createModuleLoader( self._document )
 				return loader.load_module( fullname )
-		
+
 		return self._default_load_module( fullname )
 
-					
+
 	def _default_load_module(self, fullname):
 		return self._document.newModule( fullname, self )
 
 
-class ModuleFinder (object):
-	def __init__(self, projectSubject):
+
+
+class RootFinder (object):
+	def __init__(self, projectSubject, pythonPackageName):
+		self._document = projectSubject._document
 		self._projectSubject = projectSubject
+		self._name, _, self._nameSuffix = pythonPackageName.partition( '.' )
 
-	def find_module(self, fullname, namesuffix, path, document):
-		suffix = namesuffix
-		model = self._projectSubject._model
-		modelLocation = self._projectSubject._location
-		
-		while suffix != '':
-			prefix, dot, suffix = suffix.partition( '.' )
 
-			item = model.contentsMap.get( prefix )
-
-			if item is not None:
-				modelLocation = modelLocation + '.' + prefix
-				model = item
-
-				if isinstance( model, ProjectPage ):
-					if suffix == '':
-						# We have found a page: get its subject
-						pageSubject = self._projectSubject._document.newModelSubject( model.data, self._projectSubject, modelLocation, fullname, prefix )
-						# Now, check if it has a 'createModuleLoader' method - if it has, then we can use it. Otherwise, we can't
-						try:
-							createModuleLoader = pageSubject.createModuleLoader
-						except AttributeError:
-							return None
-						else:
-							# The subject has a 'createModuleLoader' attribute - invoke it to create the module loader, for the module import system to use
-							return createModuleLoader( document )
-					else:
-						# Still path to consume; cannot go further
-						return None
-				elif isinstance( model, ProjectPackage ):
-					if suffix == '':
-						# The import statement is attempting to import this package
-						return _PackageLoader( self._projectSubject, modelLocation, model, document )
-					else:
-						# Still path to consume; exit into the traversal while-loop
-						continue
-				else:
-					raise TypeError, 'unrecognised model type'
+	def find_module(self, name, fullname, path):
+		if name == self._name:
+			if self._nameSuffix == '':
+				return self._projectSubject._packageFinder
 			else:
-				return None
-
-		# Ran out of name to consume, ERROR
-		raise ValueError, 'Name \'%s\' consumed, target not found' % namesuffix
-
-
-	
-
-class _RootModuleLoader (object):
-	def __init__(self, document):
-		self._document = document
+				return RootFinder( self._projectSubject, self._nameSuffix )
+		else:
+			return None
 
 	def load_module(self, fullname):
 		try:
@@ -114,30 +121,3 @@ class _RootModuleLoader (object):
 			pass
 
 		return self._document.newModule( fullname, self )
-	
-
-class RootFinder (object):
-	"""
-	RootFinder is a module finder that navigates the root Python package name of a project
-	"""
-	def __init__(self, projectSubject):
-		self._projectSubject = projectSubject
-
-	def find_module(self, fullname, path, document):
-		pythonPackageName = self._projectSubject._model.pythonPackageName
-		if pythonPackageName is not None:
-			pythonPackageName = pythonPackageName.split( '.' )
-			suffix = fullname
-			index = 0
-			while suffix != '':
-				prefix, dot, suffix = suffix.partition( '.' )
-				if prefix == pythonPackageName[index]:
-					index += 1
-					if index == len( pythonPackageName )  and  suffix != '':
-						return self._projectSubject._moduleFinder.find_module( fullname, suffix, path, document )
-				else:
-					return None
-			return _RootModuleLoader( document )
-		else:
-			return None
-
