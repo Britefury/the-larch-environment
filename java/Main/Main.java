8e54bbcc6ca2d198ebb21ee14959e83204e183a0
Britefury <Britefury@localhost>
2012-05-29 09:02:02 +0100

    Larch can now run from within a stand-alone JAR. The Main class now checks to see where it was started from (file system or JAR) and invokes the appropriate function within the Britefury.app_startup module. The plugin system will now look in the appropriate place.

--- java/Main/Main.java
+++ java/Main/Main.java
@@ -6,7 +6,14 @@
 //##************************
 package Main;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.python.core.Py;
 import org.python.core.PyException;
+import org.python.core.PyObject;
+import org.python.core.__builtin__;
+import org.python.core.imp;
 import org.python.util.PythonInterpreter;
 
 public class Main
@@ -14,8 +21,44 @@ public class Main
 	public static void main(String[] args) throws PyException
 	{
 		PythonInterpreter interp = new PythonInterpreter();
-//		interp.exec( "import sys" );
-//		interp.exec( "sys.path.append( 'larch' )" );
+		URL startURL = Main.class.getResource( "Main.class" );
+		String larchClassPath = null;
+		
+		PyObject app_startup_mod = imp.importName( "Britefury.app_startup", false );
+		
+		if ( startURL.getProtocol().equals( "file" ) )
+		{
+			String path = startURL.getPath();
+			int lastSep = path.lastIndexOf( '/');
+			path = path.substring( 0, lastSep );
+			lastSep = path.lastIndexOf( '/' );
+			larchClassPath = path.substring( 0, lastSep );
+			
+			PyObject appStartupFromFileSystemFn = __builtin__.getattr( app_startup_mod, Py.newString( "appStartupFromFileSystem" ) );
+			appStartupFromFileSystemFn.__call__( Py.newString( larchClassPath ) );
+		}
+		else if ( startURL.getProtocol().equals( "jar" ) )
+		{
+			String fullPath = startURL.getPath();
+			int exclamation = fullPath.indexOf( '!' );
+			fullPath = fullPath.substring( 0, exclamation );
+			URL jarURL = null;
+			try
+			{
+				jarURL = new URL( fullPath );
+			}
+			catch (MalformedURLException e)
+			{
+				System.err.println( "Bad JAR URL" );
+				System.exit( -1 );
+			}
+
+			PyObject appStartupFromJarFn = __builtin__.getattr( app_startup_mod, Py.newString( "appStartupFromJar" ) );
+			appStartupFromJarFn.__call__( Py.java2py( jarURL ) );
+		}
+		
+		
+		// Start Larch
 		interp.exec( "from Britefury.app_larch import start_larch" );
 		interp.exec( "start_larch()" );
 	}
