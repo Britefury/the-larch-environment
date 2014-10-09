fc3d1821ccfdee5adedd5bd50164b7c9d130bf79
Geoffrey French <frondit1985@gmail.com>
2014-10-09 15:38:14 +0100

    Added a doc-string to TreeEventListener.

--- java/BritefuryJ/LSpace/TreeEventListener.java
+++ java/BritefuryJ/LSpace/TreeEventListener.java
@@ -8,5 +8,13 @@ package BritefuryJ.LSpace;
 
 public interface TreeEventListener
 {
+    /**
+     * Handle a tree event
+     *
+     * @param element - the element to which the tree event handler is attached
+     * @param sourceElement - the element at which the event originally occurred before bubbling up to `element`
+     * @param event - the event
+     * @return - a boolean indicating if the event was consumed
+     */
 	boolean onTreeEvent(LSElement element, LSElement sourceElement, Object event);
 }
