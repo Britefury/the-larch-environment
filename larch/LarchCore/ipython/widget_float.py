5e1d86ef1520e929c30e26d46974307b57dfb3e0
Geoffrey French <frondit1985@gmail.com>
2014-11-24 16:09:57 +0000

    Modified IPython front-end widgets to account for slider API changes.

--- larch/LarchCore/ipython/widget_float.py
+++ larch/LarchCore/ipython/widget_float.py
@@ -1,13 +1,19 @@
-from BritefuryJ.Incremental import IncrementalValueMonitor
-from BritefuryJ.Live import LiveValue, LiveFunction
+from BritefuryJ.Live import LiveValue
 
-from BritefuryJ.Controls import Button, Checkbox, RealSlider, ToggleButton
+from BritefuryJ.Controls import RealSlider, RealProgressBar, RealRangeSlider
 
-from BritefuryJ.Pres import Pres
-from BritefuryJ.Pres.Primitive import Blank, Label, Row, Spacer
+from BritefuryJ.Pres.Primitive import Label, Row, Spacer
 
 from LarchCore.ipython.widget import IPythonWidgetView
 
+
+
+class FloatTextView (IPythonWidgetView):
+	def __present__(self, fragment, inh):
+		self._incr.onAccess()
+
+		value = float(self.value)
+		return Row([Label(self.description), Spacer(10.0, 0.0), value])
 
 
 class FloatSliderView (IPythonWidgetView):
@@ -19,19 +25,38 @@ class FloatSliderView (IPythonWidgetView):
 			self.model.send_sync(sync_data)
 			value_live.setLiteralValue(new_value)
 
+
+		def _on_range_change(control, new_lower, new_upper):
+			_on_change(control, (new_lower, new_upper))
+
+		slider_min = float(self.min)
+		slider_max = float(self.max)
+		slider_step = float(self.step)
+		if isinstance(self.value, tuple)  or  isinstance(self.value, list):
+			value = (float(self.value[0]), float(self.value[1]))
+			value_live = LiveValue(value)
+			slider = RealRangeSlider(value_live, slider_min, slider_max, slider_step, 400.0, _on_range_change)
+		else:
+			value = float(self.value)
+			value_live = LiveValue(value)
+			slider = RealSlider(value_live, slider_min, slider_max, slider_step, 400.0, _on_change)
+		return Row([Label(self.description), Spacer(10.0, 0.0),
+			    slider,
+			    Spacer(10.0, 0.0), value_live])
+
+
+class ProgressView (IPythonWidgetView):
+	def __present__(self, fragment, inh):
+		self._incr.onAccess()
+
 		value = float(self.value)
 		value_live = LiveValue(value)
 		slider_min = float(self.min)
 		slider_max = float(self.max)
-		slider_step = float(self.step)
-		if slider_min >= 0:
-			pivot = slider_min
-		elif slider_max <= 0:
-			pivot = slider_max
-		else:
-			pivot = 0
 		return Row([Label(self.description), Spacer(10.0, 0.0),
-			    RealSlider(value_live, slider_min, slider_max, slider_step, pivot, 400.0, _on_change),
+				RealProgressBar(value_live, slider_min, slider_max, 400.0),
 			    Spacer(10.0, 0.0), value_live])
+
+
 
 
