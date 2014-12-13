27b8035aa8b508bcb04e6b0138919ef15a62f05f
Geoffrey French <frondit1985@gmail.com>
2014-12-13 11:07:01 +0000

    Fixed bug in TextView and TextareaView IPython widgets. Implemented ToggleButtonsView and DropdownView selection widgets; RadioButtonsView and SelectView remain unimplemented. Implemented TabView selection container widget; AccordioView remains to be implemented.

--- larch/LarchCore/ipython/ipy_widgets.py
+++ larch/LarchCore/ipython/ipy_widgets.py
@@ -4,4 +4,6 @@ from LarchCore.ipython.widget_button import *
 from LarchCore.ipython.widget_bool import *
 from LarchCore.ipython.widget_int import *
 from LarchCore.ipython.widget_float import *
+from LarchCore.ipython.widget_selection import *
+from LarchCore.ipython.widget_selectioncontainer import *
 from LarchCore.ipython.widget_string import *
